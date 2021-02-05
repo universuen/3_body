42e3a16c974b43bbbcd09aee6fd71da3a50eea0c
whusober <whusober@gmailcom>
2021-02-06 04:28:07 +0800

    basic "base" and "rules"

---
+++ base.py
@@ -0,0 +1,60 @@
+import math
+
+TIME_GRANULARITY = 1e-3
+
+
+class Vector:
+    def __init__(self, x: float = 0, y: float = 0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return isinstance(other, Vector) and self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        assert isinstance(other, Vector)
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        assert isinstance(other, Vector)
+        return Vector(self.x - other.x, self.y - other.y)
+
+    # scalar multiplication
+    def __mul__(self, other: float):
+        return Vector(other * self.x, other * self.y)
+
+    def __rmul__(self, other: float):
+        return self.__mul__(other)
+
+    def __truediv__(self, other: float):
+        return Vector(self.x / other, self.y / other)
+
+    def __str__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    @property
+    def norm(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+
+class Particle:
+    def __init__(self, quality: float, position: Vector, velocity: Vector):
+        self.m = quality
+        self.pos = position
+        self.v = velocity
+
+    @property
+    def momentum(self):
+        return self.m * self.v
+
+    @property
+    def kinetic_energy(self):
+        return 0.5 * self.m * (self.v.norm ** 2)
+
+    def __str__(self):
+        return f'Particle({self.m}, {self.pos}, {self.v})'
+
+
+def update_vec(vector: Vector, derivative: Vector):
+    delta = derivative * TIME_GRANULARITY
+    return vector + delta
---
+++ rules.py
@@ -0,0 +1,22 @@
+from base import Particle
+
+G = 6.67e-11
+
+
+# elastic collision
+def collision(p1: Particle, p2: Particle):
+    assert p1.pos == p2.pos
+    p1_v = (p1.v * (p1.m - p2.m) + 2 * p2.momentum) / (p1.m + p2.m)
+    p2_v = (p2.v * (p2.m - p1.m) + 2 * p1.momentum) / (p1.m + p2.m)
+    p1.v = p1_v
+    p2.v = p2_v
+
+
+# universal gravitation
+def gravitation(p1: Particle, p2: Particle):
+    r = (p1.pos - p2.pos).norm
+    if r == 0:
+        return 0
+    return G * p1.m * p2.m / r ** 2
+
+
